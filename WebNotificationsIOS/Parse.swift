c19c25563c13123d5038683f1868c82b53cc212b
guofei <guofei1986@gmail.com>
2017-01-30 00:46:48 +0900

    fix: html parse

--- WebNotificationsIOS/Parse.swift
+++ WebNotificationsIOS/Parse.swift
@@ -14,23 +14,26 @@ func parse(_ url: String?) -> (title: String?, content: String?) {
     return (nil, nil)
   }
 
-  let jiDoc = Ji(htmlURL: URL(string: url!)!)
-  if jiDoc == nil {
+  guard let url = URL(string: url!) else {
     return (nil, nil)
   }
 
-  let title = jiDoc?.xPath("//title")?.first?.content
-  var content = jiDoc?.rootNode?.content
+  guard let jiDoc = Ji(htmlURL: url) else {
+    return (nil, nil)
+  }
+
+  let title = jiDoc.xPath("//title")?.first?.content
+  var content = jiDoc.rootNode?.content
 
   if content != nil {
-    if let scripts = jiDoc?.xPath("//script") {
+    if let scripts = jiDoc.xPath("//script") {
       for item in scripts {
         if let replace = item.content {
           content = content?.replacingOccurrences(of: replace, with: "")
         }
       }
     }
-    if let style = jiDoc?.xPath("//style") {
+    if let style = jiDoc.xPath("//style") {
       for item in style {
         if let replace = item.content {
           content = content?.replacingOccurrences(of: replace, with: "")
